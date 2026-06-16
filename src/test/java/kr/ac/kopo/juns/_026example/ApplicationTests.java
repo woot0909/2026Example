cd2d26d384d457f8b2b5e59a048ac35c5d1419a3
woot0909 <jeons0680@gmail.com>
2026-06-16 10:48:55 +0900

    0616

---
+++ src/test/java/kr/ac/kopo/juns/_026example/ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.juns._026example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
