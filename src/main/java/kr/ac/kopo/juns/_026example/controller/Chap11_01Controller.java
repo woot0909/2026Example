7227f6d6cbad571525e42a2a3651a2a1cac9ec55
woot0909 <jeons0680@gmail.com>
2026-08-18 10:59:32 +0900

    @ControllerAdvice와 @ExceptionHandler를 사용한 예외처리 방법

--- src/main/java/kr/ac/kopo/juns/_026example/controller/Chap11_01Controller.java
+++ src/main/java/kr/ac/kopo/juns/_026example/controller/Chap11_01Controller.java
@@ -1,8 +1,10 @@
 package kr.ac.kopo.juns._026example.controller;
 
-import kr.ac.kopo.juns._026example.exception.UserExeption;
+import kr.ac.kopo.juns._026example.exception.UserException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -10,15 +12,31 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @Controller
 @RequestMapping("/exam11_01")
 public class Chap11_01Controller {
-    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "URI 요청이 잘못되었습니다.")
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "요청이 잘못되었습니다.")
     @GetMapping("/exam01")
     public void requestMethod01(){
         System.out.println(new IllegalArgumentException("요청에 실패했습니다.").getMessage());
-
     }
 
     @GetMapping("/exam02")
-    public void requestMethod02() throws Exception {
-        throw new Exception(new UserExeption("UserException 메시지입니다."));
+    public void requestMethod02() throws Exception{
+        throw new Exception(new UserException("UserException 메시지입니다."));
+    }
+
+    @GetMapping("/exam03")
+    public void requestMethod03(){
+        throw new UserException("UserException @ExceptionHandler 예제 메시지입니다.");
+    }
+
+//    @ExceptionHandler(UserException.class)
+//    public String handlerException(UserException ex, Model model){
+//        model.addAttribute("data1", ex.getMessage());
+//        model.addAttribute("data2", ex.toString());
+//        return "viewPage11_01";
+//    }
+
+    @GetMapping("/exam04")
+    public void requestMethod04(){
+        throw new UserException("UserException @ControllerAdvice 예제 메시지입니다.");
     }
 }
